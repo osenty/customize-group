a531045272228ef4b11cd9bb722fef9f5e865ea4
osenty <user@mail.ru>
2025-04-12 22:46:25 +0300

    update

--- src/main/java/org/example/menu/action/item/MenuItem.java
+++
@@ -1,9 +0,0 @@
-package org.example.menu.action.item;
-
-import org.bukkit.event.inventory.InventoryClickEvent;
-
-public interface MenuItem {
-    void handle(InventoryClickEvent event);
-}
-
-
--- src/main/java/org/example/menu/action/parser/MenuActionParser.java
+++
@@ -1,9 +0,0 @@
-package org.example.menu.action.parser;
-
-import org.example.menu.action.item.MenuItem;
-
-public interface MenuActionParser {
-    boolean matches(String raw);
-    MenuItem parse(String raw);
-}
-
